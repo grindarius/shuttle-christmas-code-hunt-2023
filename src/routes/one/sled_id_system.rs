98197984bb1a50273af710e0b58b8c5ad043a20b
Bhattarapong Somwong <numbbutt34685@gmail.com>
2023-12-01 23:48:19 +0700

    support negative numbers

--- src/routes/one/sled_id_system.rs
+++ src/routes/one/sled_id_system.rs
@@ -3,10 +3,10 @@ use axum::{extract::Path, http::StatusCode, response::IntoResponse};
 pub async fn handler(Path(path): Path<String>) -> impl IntoResponse {
     let path_numbers = path
         .split_terminator("/")
-        .map(|p| p.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .map(|p| p.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
 
-    let mut starting_number: u32 = 0;
+    let mut starting_number: i32 = 0;
     for number in path_numbers.iter() {
         starting_number = starting_number ^ number;
     }
